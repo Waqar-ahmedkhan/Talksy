98542d5554e5e4b36ff94f0b46bdefa34834b0c4
Waqar ahmed <waqarahmed44870@gmail.com>
2025-10-04 16:36:49 +0500

    commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -35,7 +35,7 @@ export const requestOtp = async (req, res) => {
     );
 
     // Optional: Hide OTP in production response
-    const responseOtp = process.env.NODE_ENV === "production" ? "******" : otp;
+    // const responseOtp = process.env.NODE_ENV === "production" ? "******" : otp;
 
     return res.json({
       success: true,
