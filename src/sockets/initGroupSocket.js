8fea8a5f05085e96140d0d1b6f6b8694cb81cbcf
Waqar-ahmedkhan <waqarahmed44870@gmail.com>
2025-12-18 23:26:53 +0500

    commit

--- src/sockets/initGroupSocket.js
+++ src/sockets/initGroupSocket.js
@@ -9,9 +9,7 @@ import { isValidObjectId } from "mongoose";
 import mongoose from "mongoose";
 
 
-export function getGroupRoom(groupId) {
-  return `group_${groupId}`;
-}
+export const getGroupRoom = (groupId) => `group_${groupId}`;
 
 const logTimestamp = () =>
   moment().tz("Asia/Karachi").format("DD/MM/YYYY, hh:mm:ss a");
